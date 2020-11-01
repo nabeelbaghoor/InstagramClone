e781ec08248dc7d05cf9ab2a34bb510150a7f027
nabeelbaghoor <60441195+nabeelbaghoor@users.noreply.github.com>
2020-11-01 09:02:34 +0500

    initial commit

---
+++ src/DB_Firebase/company/Main.java
@@ -0,0 +1,38 @@
+package DB_Firebase.company;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("C:\\Users\\nabeelbaghoor\\IdeaProjects\\InstagramClone\\instagramclone-58441-firebase-adminsdk-taebo-2cba3bad0c.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://instagramclone-58441.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        /*// As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                System.out.println(document);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });*/
+    }
+}
