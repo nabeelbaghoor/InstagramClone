a59aa7a4013738f32ae7b421ffd5ffdc85e850e6
Ahmad Hamza <ahmadhamzanaqvi@gmail.com>
2020-11-05 23:01:31 +0500

    json library included

--- src/DB_Text/DB_text.java
+++ src/DB_Text/DB_text.java
@@ -8,12 +8,4 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
-public class DB_text implements IDB_Operations {
-    public void initDB() throws IOException{}
-    public IModel getObject(String objectId, ModelType modelType) throws ExecutionException, InterruptedException; //returns object with key == objectId
-    public ArrayList<IModel> getObjectsList(ArrayList<String> objectIds, ModelType modelType) throws ExecutionException, InterruptedException; //returns objects list with keys matching objectIds
-    public String addObject(IModel object,ModelType modelType) throws ExecutionException, InterruptedException;   //returns objectId of new created object
-    public boolean removeObject(String objectId,ModelType modelType) throws ExecutionException, InterruptedException;  //removes object , and returns boolean
-    public boolean updateObject(String objectId, HashMap<String,String> attributesToBeUpdated, ModelType modelType); //overwrites the object with key == objectId
 
-}
--- src/DB_Text/Main.java
+++ src/DB_Text/Main.java
@@ -1,7 +1,7 @@
 package DB_Text;
-import org.json.simple.JSONObject;
+
 public class Main {
     public static void main() {
-        System.out.println("maza");
+        System.out.println("  ");
     }
 }
