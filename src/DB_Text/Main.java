48ff3bb3c0187e9d52b4ac1d90064ccc203990a2
Ahmad Hamza <ahmadhamzanaqvi@gmail.com>
2020-11-05 21:05:48 +0500

    json library included

--- src/DB_Text/Main.java
+++ src/DB_Text/Main.java
@@ -1,7 +1,7 @@
 package DB_Text;
-
+import org.json.simple.JSONObject;
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main() {
+        System.out.println("maza");
     }
 }
