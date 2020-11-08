45d69179859fd9f7d4927bd9b2d10a4c5fb8cd33
nabeelbaghoor <60441195+nabeelbaghoor@users.noreply.github.com>
2020-11-08 16:26:16 +0500

    made factory ,cfg filepath relativve

--- src/UI_Console/Main.java
+++ src/UI_Console/Main.java
@@ -2,10 +2,7 @@ package UI_Console;
 
 import Models.Factory;
 import Models.Operations;
-import Models.Post;
 import Models.User;
-
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -22,24 +19,32 @@ public class Main {
          */
 
         //add a post by current(Logged in) user
-        operations.addPost(".\\Images\\s.png",
-                "Always bear in mind that your own resolution to succeed is more important than any other.");
 
+        /*
+        operations.addPost(".\\Images\\s.png",
+              "Always bear in mind that your own resolution to succeed is more important than any other.");
+        */
 
         //get current(Logged in) user posts
+        /*
         ArrayList<Post> posts = operations.getNewsFeedPosts(user.getID());
         for(Post post: posts){
             post.print();
         }
+        */
 
         //edit current(Logged in) user's Profile
+
+        /*
         user.print();   //user data before edit
+
         user.phoneNumber = "00000000000";
         user.username = "nabeelhassan";
         operations.editUserData(user);
 
         user = operations.getMyProfile();
         user.print();   //user data after edit
+        */
 
     }
 }
