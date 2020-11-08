6c700939c54550a5cb8c7ac4746ea0bb2b5296f3
nabeelbaghoor <60441195+nabeelbaghoor@users.noreply.github.com>
2020-11-08 13:35:15 +0500

    shifted from java.jar to maven

--- src/UI_Console/Main.java
+++ src/UI_Console/Main.java
@@ -9,10 +9,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Factory func = new Factory();
         Operations operations = func.getOperations();
+
+        //add a post
+
+        //get user posts
         User user = operations.getMyProfile();
         for(Post post: operations.getNewsFeedPosts(user.getID())){
             post.print();
         }
-        //operations.addPost(".\\Images\\s.png","Give second chances but not for the same mistake");
     }
 }
