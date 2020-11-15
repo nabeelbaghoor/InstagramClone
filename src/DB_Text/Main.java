4dbe76fb57fd12e1effd5140fb70065a4d9699d4
Ahmad Hamza <ahmadhamzanaqvi@gmail.com>
2020-11-15 21:05:44 +0500

    Sema

--- src/DB_Text/Main.java
+++ src/DB_Text/Main.java
@@ -10,7 +10,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //CreateDummyDB();
+        CreateDummyDB();
     }
 
     public static void CreateDummyDB() throws Exception {
